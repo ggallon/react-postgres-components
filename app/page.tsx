37f26bd630efb156a6929527134b4b8d04a65f09
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-01-26 21:38:22 +0100

    Move all icons to icons components

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,14 @@ import { Question } from "./question";
 import Link from "next/link";
 import { Github } from "lucide-react";
 
+import { Elephant, SourceIcon } from "@/components/icons";
+
 export default function Home() {
   return (
     <main className="px-5 md:px-10 md:py-5 max-w-3xl">
       <div className="my-5">
         <Link href="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="69"
-            height="43"
-            fill="none"
-          >
-            <path
-              fill="#000"
-              d="m6.74 14.9 16.47-6.12 1.27-6.52A2.68 2.68 0 0 1 27.1.08h20.55c1.24 0 2.35.79 2.77 1.95l3.54 9.98 2.32-2.24a4.22 4.22 0 0 1 2.93-1.18h3.84c1.15 0 2.24.55 2.91 1.48l1.65 2.27c.74 1 .75 2.36.05 3.38l-.8 1.16a2.37 2.37 0 0 1-3.36.58l-2.17-1.58-3.7 5.07a3.18 3.18 0 0 1-3.08 1.27l-10.27-1.66-1.25 1.16v18.77a2.43 2.43 0 0 1-2.42 2.43h-5.64c-1.08 0-2.13-.4-2.93-1.14l-9.7-8.83-4.86 8.32a3.34 3.34 0 0 1-2.88 1.65H7.23a2.42 2.42 0 0 1-2.42-2.43v-9.14L1.46 28a2.21 2.21 0 0 1 .34-3.4l3.02-2.02v-4.92c0-1.23.77-2.34 1.92-2.78v.01ZM41 20.33l1.18-1.09-15.55-15.5-1.19 6.15-1.85 9.6 11.98 5.87 5.41-5.01.02-.02Zm7.17-17.5a.53.53 0 0 0-.5-.35H28.8l15.69 15.67 9.66 1.56-5.98-16.88ZM64.9 15.52l.8-1.15a.55.55 0 0 0 0-.6l-1.4-1.93-1.53 2.1 2.15 1.58h-.02ZM61.92 11h-2.7c-.46 0-.92.18-1.26.5l-3.12 3 1.48 4.17L61.91 11ZM33.68 40c.36.32.83.5 1.31.5l5.66-.03V23.94l-4.04 3.74c-.38.35-.92.41-1.35.2L21.7 21.22c-.5-.24-.76-.77-.65-1.3l1.62-8.4-14.39 5.35 25.4 23.11v.01Zm-26.44.5h7.38c.33 0 .64-.18.8-.46l5.11-8.76L7.21 19.15V40.5h.03ZM3.35 26.47l1.48 1.48v-2.47l-1.48.99Z"
-            />
-          </svg>
+          <Elephant />
         </Link>
       </div>
 
@@ -37,7 +29,7 @@ export default function Home() {
 
         <pre className="my-5 bg-gray-100 p-5 pt-4 rounded-md overflow-auto">
           <p className="text-gray-500 text-sm mb-2">
-            <SourceIcon />
+            <SourceIcon className="mr-1 inline-block" />
             rpc/hello-world.tsx
           </p>
           <code
@@ -440,23 +432,5 @@ $$ LANGUAGE plv8 IMMUTABLE STRICT;`),
         </p>
       </div>
     </main>
-  );
-}
-
-function SourceIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="12"
-      height="12"
-      className="inline-block mr-1"
-      viewBox="-3 0 32 32"
-    >
-      <path
-        style={{ fill: "currentColor" }}
-        fillRule="evenodd"
-        d="M20 8a2 2 0 0 1-2-2V2l6 6h-4Zm4 20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h11.972C15.954 4.395 16 6 16 6a4 4 0 0 0 4 4h4v18ZM18 0v.028c-.128 0-.662-.049-2-.028H4a4 4 0 0 0-4 4v24a4 4 0 0 0 4 4h18a4 4 0 0 0 4-4V8l-8-8Z"
-      />
-    </svg>
   );
 }
